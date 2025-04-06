e19fd06bb3df4e4418e83ccd5ce3ba86b4c6ba89
KarimPathan <karim.pathan@stud.hslu.ch>
2025-04-06 11:14:40 +0200

    animations

--- sprint_2/script.js
+++ sprint_2/script.js
@@ -3,5 +3,9 @@ window.addEventListener('DOMContentLoaded', () => {
         document.body.offsetHeight;
       document.body.classList.remove('intro-mode');
       document.body.classList.add('main-mode');
+      setTimeout(() => {
+        document.body.style.opacity = 1;
+    }, 10);
     }, 3000);
+
   });
